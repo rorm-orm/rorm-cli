f896dddb187255085cd6946343e26d94cc9db993
myOmikron <git@omikron.dev>
2022-12-21 13:45:20 +0100

    Bumped rorm-sql

--- src/utils/bind.rs
+++ src/utils/bind.rs
@@ -1,6 +1,8 @@
 use rorm_sql::value;
+use rorm_sql::value::NullType;
 use sqlx::database::HasArguments;
 use sqlx::query::Query;
+use sqlx::types::chrono::{NaiveDate, NaiveDateTime, NaiveTime};
 
 type AnyQuery<'q> = Query<'q, sqlx::Any, <sqlx::Any as HasArguments<'q>>::Arguments>;
 
@@ -26,10 +28,19 @@ where
         value::Value::NaiveDate(x) => query.bind(x),
         value::Value::NaiveTime(x) => query.bind(x),
         value::Value::NaiveDateTime(x) => query.bind(x),
-        value::Value::Null => {
-            static NULL: Option<bool> = None;
-            query.bind(NULL)
-        }
+        value::Value::Null(null_type) => match null_type {
+            NullType::String => query.bind(None::<&str>),
+            NullType::I64 => query.bind(None::<i64>),
+            NullType::I32 => query.bind(None::<i32>),
+            NullType::I16 => query.bind(None::<i16>),
+            NullType::Bool => query.bind(None::<bool>),
+            NullType::F64 => query.bind(None::<f64>),
+            NullType::F32 => query.bind(None::<f32>),
+            NullType::Binary => query.bind(None::<&[u8]>),
+            NullType::NaiveTime => query.bind(None::<NaiveTime>),
+            NullType::NaiveDate => query.bind(None::<NaiveDate>),
+            NullType::NaiveDateTime => query.bind(None::<NaiveDateTime>),
+        },
         value::Value::Ident(_) => query,
         value::Value::Column { .. } => query,
     }
